804fc6fbc20aced21ed3a5cf2997fb07be55a696
Jorge Avila <jorgefavilaoliveira@gmail.com>
2024-11-18 18:41:55 +0000

    modificando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mayara, eu amo voce. Se voce me ama me da uma risadinha.</h1>
+    return <h1>Mayara, eu amo voce. Se voce me ama me da uma risadinha!</h1>
 }
 
 export default Home;
