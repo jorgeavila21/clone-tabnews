33631d6f64cad3e3def13177c8381220e5fbdf80
Jorge Avila <jorgefavilaoliveira@gmail.com>
2024-11-25 19:22:42 +0000

    adiciona `.prettierignore´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-function Home() {
-  return <h1>Mayara, eu amo voce. Se voce me ama me da uma risadinha!</h1>;
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <img src="https://1drv.ms/i/c/bf6951484e97967e/Eelr6yLh3xZLihtBADbK-UsB-soeTXrrVBxVCOl8AYig5Q?e=8NS5aS" alt="Sweet Brownies Logo" className="logo" />
+      </header>
+      <main>
+        <h1>Em construção</h1>
+        <div className="image-placeholder">
+          <img src="https://cf.girlsaskguys.com/q5071483/5714aa16-50ab-4066-9153-b0168c167e7a.gif" alt="em construção" className='comendoBrownie' />
+        </div>
+        <div className="loading-bar">
+          <div className="loading-progress"></div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Home;
+
+export default App;
+
