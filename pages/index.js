436c76fb2ea327a3d52425fc6f2c44aabfc77612
Jorge Avila <jorgefavilaoliveira@gmail.com>
2024-11-27 17:09:03 +0000

    alterações no `index` e `css`'

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React from 'react'; 
 import './index.css';
 
-function App() {
-  return (
-    <div className="container">
-      <header>
-        <img src="https://1drv.ms/i/c/bf6951484e97967e/Eelr6yLh3xZLihtBADbK-UsB-soeTXrrVBxVCOl8AYig5Q?e=8NS5aS" alt="Sweet Brownies Logo" className="logo" />
-      </header>
-      <main>
-        <h1>Em construção</h1>
-        <div className="image-placeholder">
-          <img src="https://cf.girlsaskguys.com/q5071483/5714aa16-50ab-4066-9153-b0168c167e7a.gif" alt="em construção" className='comendoBrownie' />
-        </div>
-        <div className="loading-bar">
-          <div className="loading-progress"></div>
-        </div>
-      </main>
-    </div>
-  );
-}
+function Home() { 
+  return ( 
+  <div className="container"> 
+    <header> 
+      <img src="https://1drv.ms/i/c/bf6951484e97967e/Eelr6yLh3xZLihtBADbK-UsB-soeTXrrVBxVCOl8AYig5Q?e=nWo3O8" alt="Sweet Brownies Logo" className="logo" />
+    </header>
+  <div style={{ textAlign: 'center', marginTop: '20%' }}> 
+    <img src="https://cf.girlsaskguys.com/q5071483/5714aa16-50ab-4066-9153-b0168c167e7a.gif" alt="Pagina em construção" className='image-placeholder' style={{ width: '100px', height: '100px' }} /> 
+    <h1 style={{ color: '#6563ff' }}>Página em Construção</h1> </div> 
+  </div>
+    ); 
+  }
 
-
-export default App;
-
+export default Home
