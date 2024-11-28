72570fb684003eee91fe1deede311bcf95234486
Jorge Avila <jorgefavilaoliveira@gmail.com>
2024-11-28 00:59:11 +0000

    exclui a pasta '`styles`'

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import React from 'react'; 
-import './index.css';
 
 function Home() { 
   return ( 
   <div className="container"> 
     <header> 
-      <img src="https://1drv.ms/i/c/bf6951484e97967e/Eelr6yLh3xZLihtBADbK-UsB-soeTXrrVBxVCOl8AYig5Q?e=nWo3O8" alt="Sweet Brownies Logo" className="logo" />
+      <img src="https://1drv.ms/i/c/bf6951484e97967e/Eelr6yLh3xZLihtBADbK-UsB-soeTXrrVBxVCOl8AYig5Q?e=nWo3O8" alt="Sweet Brownies Logo" />
     </header>
   <div style={{ textAlign: 'center', marginTop: '20%' }}> 
-    <img src="https://cf.girlsaskguys.com/q5071483/5714aa16-50ab-4066-9153-b0168c167e7a.gif" alt="Pagina em construção" className='image-placeholder' style={{ width: '100px', height: '100px' }} /> 
+    <img src="https://cf.girlsaskguys.com/q5071483/5714aa16-50ab-4066-9153-b0168c167e7a.gif" alt="Pagina em construção"  style={{ width: '100px', height: '100px' }} /> 
     <h1 style={{ color: '#6563ff' }}>Página em Construção</h1> </div> 
   </div>
     ); 
   }
 
-export default Home
+export default Home;
